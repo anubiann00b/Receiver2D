d7b8c6b4258879685a684e87f001ca2776fdb1fb
Prince781 <princetonf@optonline.net>
2013-10-06 23:18:41 -0400

    Changes.

--- src/com/remote/remote2d/editor/Editor.java
+++
@@ -1,7 +0,0 @@
-package com.remote.remote2d.editor;
-
-public class Editor {
-	public static void run() {
-		
-	}
-}
--- src/com/remote/remote2d/engine/Console.java
+++
@@ -1,9 +0,0 @@
-package com.remote.remote2d.engine;
-
-import com.remote.remote2d.engine.Receiver2D;
-
-public class Console {
-	public static void log(String message) {
-		System.out.println(Receiver2D.getTimeSinceLaunch()+" "+message);
-	}
-}
