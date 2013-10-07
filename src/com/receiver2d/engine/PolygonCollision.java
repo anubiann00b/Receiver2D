8167c70507df5e42088d123b3024ac7ec7f1cd2d
Prince781 <princetonf@optonline.net>
2013-10-07 01:36:36 -0400

    Update.

--- src/com/receiver2d/engine/PolygonCollision.java
+++ src/com/receiver2d/engine/PolygonCollision.java
@@ -3,7 +3,8 @@ package com.receiver2d.engine;
 public class PolygonCollision {
 	/**
 	 * Checks whether two polygons collide.
-	 * Polygons are defined as an array of vectors with each vector connected to the next one in the array.
+	 * Polygons are defined as an array of vectors with each vector connected to
+	 * the next one in the array.
 	 * Example: square - (0,0) (0,1) (1,1) (1,0)
 	 * 
 	 * @param polyA
@@ -13,15 +14,15 @@ public class PolygonCollision {
 	 * @return the position where the two polygons collide, or null if they
 	 *         don't
 	 */
-	public static Vec2d checkCollide(Vec2d[] polyA, Vec2d[] polyB) {
+	public static Vector2D checkCollide(Vector2D[] polyA, Vector2D[] polyB) {
 		
 		// line from point poly[i] to poly[i+1] (wraps around)
 		for (int edgeA = 0; edgeA < polyA.length; edgeA++) {
-			Vec2d a1 = polyA[edgeA];
-			Vec2d a2 = polyA[(edgeA == polyA.length ? 0 : edgeA + 1)];
+			Vector2D a1 = polyA[edgeA];
+			Vector2D a2 = polyA[(edgeA == polyA.length ? 0 : edgeA + 1)];
 			for (int edgeB = 0; edgeB < polyB.length; edgeB++) {
-				Vec2d b1 = polyB[edgeB];
-				Vec2d b2 = polyB[(edgeB == polyB.length ? 0 : edgeB + 1)];
+				Vector2D b1 = polyB[edgeB];
+				Vector2D b2 = polyB[(edgeB == polyB.length ? 0 : edgeB + 1)];
 				
 				//calculations
 				
--- src/com/receiver2d/engine/Vector2D.java
+++ src/com/receiver2d/engine/Vector2D.java
@@ -1,10 +1,10 @@
 package com.receiver2d.engine;
 
-public class Vec2d {
+public class Vector2D {
 	public float x = 0.0f;
 	public float y = 0.0f;
 	
-	public Vec2d(float x, float y) {
+	public Vector2D(float x, float y) {
 		this.x = x;
 		this.y = y;
 	}
