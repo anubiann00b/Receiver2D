64d6a816e0dfa302e5349ea0b832d2a93aa756d6
Prince781 <princetonf@optonline.net>
2013-10-10 21:48:55 -0400

    Removing R2DFile class.

--- src/com/receiver2d/engine/io/R2DFile.java
+++
@@ -1,9 +0,0 @@
-package com.receiver2d.engine.io;
-
-/**
- * A specific type of file containing scene information. In the filesystem, this
- * file has the extension .r2ds
- */
-public class R2DFile {
-
-}
